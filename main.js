6cb22dc484dc930c3cb991d13d9dac770e4549c9
Carolina Leite Cardinale <96205213+carolinacardinale@users.noreply.github.com>
2023-09-13 10:27:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function calcular(){
+    var idade = document.getElementById("idade").value
+
+    if(idade>=18){
+        alert("Você já pode dirigir")
+
+    }
+    else{
+        alert("Você não pode dirigir!")
+    }
+}
